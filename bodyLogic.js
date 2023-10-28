29decfd10760046d3c1f81c72d64563bee84f3ef
Abhishek <gopalakrishnanunni.a@northeastern.edu>
2023-10-27 20:09:33 -0400

    initial commit

---
+++ bodyLogic.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var checkBox = document.getElementById('toggleSections');
+    document.getElementById('toggleSections').addEventListener('change', function() {
+        var mapSection = document.getElementById('mapSection');
+        var cardSection = document.getElementById('cardSection');
+        
+        if (this.checked) {
+            mapSection.style.display = 'block';
+            cardSection.style.display = 'none';
+        } else {
+            mapSection.style.display = 'none';
+            cardSection.style.display = 'block';
+        }
+    });
+    });
