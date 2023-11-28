3600129a0ca2df315b1a84fb56c0ee2222762247
Anirudha5997 <dudhasagare.a@northeastern.edu>
2023-11-28 17:16:56 -0500

    added common.js

---
+++ frontend/src/Common/common.js
@@ -0,0 +1,5 @@
+export const getFromLocalStorage = (key) => localStorage.getItem(key) || "";
+export const setLocalStorage = (key, value) => localStorage.setItem(key, value);
+export const emailRegexp = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+export const passwordRegexp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%#*?&]{8,}$/;
+export const capitalizeFirstLetter = (str) => str.split(' ').map(ele => ele.charAt(0).toUpperCase() + ele.slice(1).toLowerCase()).join(' ')
