f7c069f073fc6b24529aa16d1e72f44289f76e63
Daryl Fernandes <fernandes.dar@northeastern.edu>
2023-12-07 23:54:26 -0500

    Commented memory server code

--- backend/database/connection.js
+++ backend/database/connection.js
@@ -1,11 +1,17 @@
 const mongoose = require("mongoose")
+const dotenv = require('dotenv');
 const { MongoMemoryServer } = require("mongodb-memory-server")
 
+dotenv.config();
+
 const connect = async () => {
+    const connectionString = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/";
+    const database = process.env.DATABASE || "test";
+
     const mongoServer = await MongoMemoryServer.create()
     const mongoUri = mongoServer.getUri()
-    // await mongoose.connect(`${connectionString}${database}`)
-    await mongoose.connect(mongoUri, { dbName: "imageTest" })
+    await mongoose.connect(`${connectionString}${database}`)
+    // await mongoose.connect(mongoUri, { dbName: "imageTest" })
 }
 
 module.exports = { connect }
